7e7fbdb859bac60e6aa75199af4bff49bbc40469
Edgardo Blanco <edblanco@indec.gob.ar>
2024-02-27 15:30:59 -0300

    se registra nuevo mensaje al existir inconsistencias en la importacion hasta el momento se accede mediante url /cargarSegmentos

--- app/Imports/SegmentosImport.php
+++ app/Imports/SegmentosImport.php
@@ -20,41 +20,46 @@ class SegmentosImport implements ToModel, WithHeadingRow, WithBatchInserts, With
     use Importable;
 
     protected $errores = [];
+    protected $errorMostrado = false;
+    protected $contadorRegistros = 0;
 
-    public function model(array $row)
-    {
-        try {
-            // Map missing or incorrect fields to expected names
-            $data = [
-                'prov' => $row['prov'],
-                'nom_prov' => $row['nom_prov'] ?? $row['nomprov'] ,
-                'dpto' => $row['dpto'] ?? $row['depto'] ,
-                'nom_dpto' => $row['nom_dpto'] ?? $row['nomdepto'] ,
-                'codloc' => $row['codloc'] ?? null,
-                'nom_loc' => $row['nom_loc'] ?? $row['nomloc'] ,
-                'codent' => $row['codent'] ?? '1',
-                'nom_ent' => $row['nom_ent'] ?? '1',
-                'frac' => $row['frac'] ,
-                'radio' => $row['radio'],
-                'tipo' => $row['tipo'] ?? 'I',
-                'seg' => $row['seg'] ,
-                'vivs' => $row['vivs'] ?? $row['viviendas'] ?? '1',
-            ];
-            return new Segmento($data);
-        } catch (\ErrorException $e) {
-            dd ($e);
-            Log::error('Error durante la importación: ' . $e);
-            // Obtener información sobre el campo y el valor recibido
-            $campo = $e->getMessage(); // Puede ser necesario un análisis más específico del mensaje de error
-            $valorRecibido = 'texto'; //$row[$campo]'';
-            // Agregar un mensaje a la colección de errores
-            $this->errores[] = "Error en el campo '$campo': valor recibido '$valorRecibido' no es válido.";
-            // Retornar un valor nulo para omitir esta fila
-            flash('error en el '. $campo  )-> error();
-            return null;
+public function model(array $row)
+{
+    try {
+        // Map missing or incorrect fields to expected names
+        $data = [
+            'prov' => $row['prov'],
+            'nom_prov' => $row['nom_prov'] ?? $row['nomprov'] ,
+            'dpto' => $row['dpto'] ?? $row['depto'] ,
+            'nom_dpto' => $row['nom_dpto'] ?? $row['nomdepto'] ,
+            'codloc' => $row['codloc'] ?? null,
+            'nom_loc' => $row['nom_loc'] ?? $row['nomloc'] ,
+            'codent' => $row['codent'] ?? '1',
+            'nom_ent' => $row['nom_ent'] ?? '1',
+            'frac' => $row['frac'] ,
+            'radio' => $row['radio'],
+            'tipo' => $row['tipo'] ?? 'I',
+            'seg' => $row['seg'] ,
+            'vivs' => $row['vivs'] ?? $row['viviendas'] ?? '1',
+        ];
+        $this->contadorRegistros++; //debe informar cantidad de registros importados exitosamente
+        return new Segmento($data);
+    } catch (\ErrorException $e) {
+        Log::error('Error durante la importación: ' . $e);
+        // Obtener información sobre el campo y el valor recibido
+        $campo = $e->getMessage();
+        $valorRecibido = $row[$campo] ?? 'No proporcionado';
+        // Agregar un mensaje a la colección de errores
+        $this->errores[] = "Error en el campo '$campo': valor recibido '$valorRecibido' no es válido.";
+        // Retornar un valor nulo para omitir esta fila
+        if (!$this->errorMostrado) { // Solo mostrar el error si no se ha mostrado antes
+            flash('Error en el campo '. $campo . ': valor recibido ' . $valorRecibido . ' no es válido')->error();
+            $this->errorMostrado = true; // Marcar que ya se ha mostrado un error
         }
+        return null;
     }
-
+    
+}
     public function batchSize(): int
     {
         return 1000;
@@ -76,5 +81,15 @@ class SegmentosImport implements ToModel, WithHeadingRow, WithBatchInserts, With
     public function getErrores(): array
     {
         return $this->errores;
+    }
+    public function getContadorRegistros():int
+    {
+        return $this->contadorRegistros;
+    }
+    public function mostrarMensajeExito()
+    {
+        if ($this->contadorRegistros > 0 && !$errorMostrado) {
+            flash ('Proceso eixtoso. Se importaron ' . $this->contadorRegistros . ' registros.' ) ->success();
+        }
     }
 }
